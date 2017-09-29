e54fe2fb7b9a99a3aa270864713fedd033001e77
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-09-29 16:02:17 +0200

    Update exercice4

--- exercice4.php
+++ exercice4.php
@@ -1,9 +1,16 @@
 <?php
 for($i = 1; $i < 101; $i++){
-echo $i;
-
-if ($i % 3 == 0){
-  echo "fizz";
-}
-}
- ?>
+  if (($i % 5 == 0) and ($i % 3 == 0)) {
+      echo "FizzBuzz ";
+    }
+    elseif ($i % 5 == 0) {
+      echo "Buzz ";
+    }
+    elseif ($i % 3 == 0) {
+      echo "Fizz ";
+    }
+    else {
+      echo $i .' ';
+    }
+  }
+   ?>
